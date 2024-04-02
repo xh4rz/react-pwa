eff01dccff38025f12bbba0dedbbcbaf6eb4930d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-02 16:22:37 -0500

    205. Workbox Cache Strategies

--- src/sw-template.js
+++ src/sw-template.js
@@ -3,3 +3,20 @@ importScripts(
 );
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+
+registerRoute(
+	new RegExp(
+		'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+	),
+	new CacheFirst()
+);
+
+registerRoute(
+	new RegExp(
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+	),
+	new CacheFirst()
+);
