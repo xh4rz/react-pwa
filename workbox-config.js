e9214dc6425ed59177777a21dc67958358a0457b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-02 15:57:56 -0500

    204. Workbox SW Manual

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
 module.exports = {
 	globDirectory: 'build/',
-	globPatterns: [
-		'**/*.{json,ico,html,png,txt,css,js}'
-	],
+	globPatterns: ['**/*.{json,ico,html,png,txt,css,js}'],
+	// ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	swSrc: 'src/sw-template.js' // generateSW no funciona con esta propiedad
 };
