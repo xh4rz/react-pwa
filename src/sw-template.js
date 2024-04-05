2bdb578a5f7a34bc2b1148f61c760195b60e3f23
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-05 00:27:30 -0500

    209. Optimizar nuestro service worker

--- src/sw-template.js
+++ src/sw-template.js
@@ -10,29 +10,52 @@ const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(
-	new RegExp(
-		'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
-	),
-	new CacheFirst()
-);
+const cacheFirstNetwork = [
+	'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+];
 
-registerRoute(
-	new RegExp(
-		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
-	),
-	new CacheFirst()
-);
+registerRoute(({ request, url }) => {
+	// console.log({ request, url });
 
-registerRoute(
-	new RegExp('http://localhost:4000/api/auth/renew'),
-	new NetworkFirst()
-);
+	if (cacheFirstNetwork.includes(url.href)) return true;
 
-registerRoute(
-	new RegExp('http://localhost:4000/api/events'),
-	new NetworkFirst()
-);
+	return false;
+}, new CacheFirst());
+
+// registerRoute(
+// 	new RegExp(
+// 		'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+// 	),
+// 	new CacheFirst()
+// );
+
+// registerRoute(
+// 	new RegExp(
+// 		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+// 	),
+// 	new CacheFirst()
+// );
+
+const cacheNetworkFirst = ['/api/auth/renew', '/api/events'];
+
+registerRoute(({ request, url }) => {
+	// console.log({ request, url });
+
+	if (cacheNetworkFirst.includes(url.pathname)) return true;
+
+	return false;
+}, new NetworkFirst());
+
+// registerRoute(
+// 	new RegExp('http://localhost:4000/api/auth/renew'),
+// 	new NetworkFirst()
+// );
+
+// registerRoute(
+// 	new RegExp('http://localhost:4000/api/events'),
+// 	new NetworkFirst()
+// );
 
 // Posteos offline
 const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
